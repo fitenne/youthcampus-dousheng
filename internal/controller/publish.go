6e502e21c999e21a9dcac674b7c63545c4cf3a57
yty <13293209977@163.com>
2022-06-03 23:43:09 +0800

    实现video数据写入数据库

--- internal/controller/publish.go
+++ internal/controller/publish.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"fmt"
 	"github.com/fitenne/youthcampus-dousheng/internal/common/jwt"
+	"github.com/fitenne/youthcampus-dousheng/internal/service"
 	"github.com/fitenne/youthcampus-dousheng/pkg/model"
 	"github.com/gin-gonic/gin"
 	"log"
@@ -19,15 +20,12 @@ type VideoListResponse struct {
 func Publish(c *gin.Context) {
 	// 判断用户是否存在
 	token := c.PostForm("token")
-	//if _, exist := usersLoginInfo[token]; !exist {
-	//	c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
-	//	return
-	//}
 	myClaims, exist := jwt.ParseToken(token)
 	if exist != nil {
 		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
 		return
 	}
+	userID := myClaims.UserID
 
 	//
 	data, err := c.FormFile("data")
@@ -39,13 +37,16 @@ func Publish(c *gin.Context) {
 		return
 	}
 
+	// 存入数据库
 	filename := filepath.Base(data.Filename)
 	log.Println("publish func, filename:", filename)
 	// user := usersLoginInfo[token]
 	// finalName := fmt.Sprintf("%d_%s", user.Id, filename)
-	finalName := fmt.Sprintf("%d_%s", myClaims.UserID, filename)
-	saveFile := filepath.Join("./public/", finalName)
-	if err := c.SaveUploadedFile(data, saveFile); err != nil {
+	finalName := fmt.Sprintf("%d_%s", userID, filename)
+	playUrl := filepath.Join("./public/", finalName)
+
+	videoID, err := service.VideoPublish(c, data, playUrl, userID)
+	if err != nil {
 		c.JSON(http.StatusOK, Response{
 			StatusCode: 1,
 			StatusMsg:  err.Error(),
@@ -53,7 +54,8 @@ func Publish(c *gin.Context) {
 		return
 	}
 
-	log.Println(finalName + " uploaded successfully")
+	// 上传成功，返回结果
+	log.Println(videoID, finalName + " uploaded successfully")
 	c.JSON(http.StatusOK, Response{
 		StatusCode: 0,
 		StatusMsg:  finalName + " uploaded successfully",
