b3f4f0df3747bb3111f54f4031eee1b99be5fb08
陈奇廷 <1634848114@qq.com>
2022-06-03 16:25:20 +0800

    ①video controller ②video service -TODO：检查收藏

---
+++ internal/service/feed.go
@@ -0,0 +1,20 @@
+package service
+
+import (
+	"errors"
+	"github.com/fitenne/youthcampus-dousheng/internal/repository"
+	"github.com/fitenne/youthcampus-dousheng/pkg/model"
+)
+
+func Feed(user_id, latest int64) ([]*model.Video, int64, error) {
+	videos, err := repository.GetVideoCtl().GetVideoList(latest, 30)
+	if err != nil {
+		return nil, 0, err
+	}
+	if len(videos) == 0 {
+		return nil, 0, errors.New("无video")
+	}
+	next_time := videos[len(videos)-1].CreatedAt
+	//TODO: 检查是否已经被收藏
+	return videos, int64(next_time), nil
+}
