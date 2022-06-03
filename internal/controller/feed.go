1e4e3a77fd91ab16493af4aa347c14ec5241b96a
陈奇廷 <1634848114@qq.com>
2022-06-03 16:22:30 +0800

    ①video controller ②video service -TODO：检查收藏

--- internal/controller/feed.go
+++ internal/controller/feed.go
@@ -1,23 +1,69 @@
 package controller
 
 import (
+	"github.com/fitenne/youthcampus-dousheng/internal/service"
 	"github.com/fitenne/youthcampus-dousheng/pkg/model"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 	"time"
 )
 
 type FeedResponse struct {
 	Response
-	VideoList []model.Video `json:"video_list,omitempty"`
-	NextTime  int64         `json:"next_time,omitempty"`
+	VideoList []*model.Video `json:"video_list,omitempty"`
+	NextTime  int64          `json:"next_time,omitempty"`
 }
 
 // Feed same demo video list for every request
 func Feed(c *gin.Context) {
+	rid := c.Query("user_id")
+	latestTime, err := strconv.ParseInt(c.Query("latest_time"), 10, 64)
+	if err != nil {
+		latestTime = time.Now().Unix()
+	}
+	var videos []*model.Video
+	var next_time int64
+	if rid == "" {
+		videoList, nextTime, err := service.Feed(-1, latestTime)
+		if err != nil {
+			c.JSON(http.StatusOK, FeedResponse{
+				Response: Response{
+					StatusCode: http.StatusInternalServerError,
+					StatusMsg:  err.Error(),
+				},
+			})
+			return
+		}
+		videos = videoList
+		next_time = nextTime
+	} else {
+		userId, err := strconv.ParseInt(rid, 10, 64)
+		if err != nil {
+			c.JSON(http.StatusOK, FeedResponse{
+				Response: Response{
+					StatusCode: http.StatusBadRequest,
+					StatusMsg:  err.Error(),
+				},
+			})
+			return
+		}
+		videoList, nextTime, err := service.Feed(userId, latestTime)
+		if err != nil {
+			c.JSON(http.StatusOK, FeedResponse{
+				Response: Response{
+					StatusCode: http.StatusInternalServerError,
+					StatusMsg:  err.Error(),
+				},
+			})
+			return
+		}
+		videos = videoList
+		next_time = nextTime
+	}
 	c.JSON(http.StatusOK, FeedResponse{
 		Response:  Response{StatusCode: 0},
-		VideoList: DemoVideos,
-		NextTime:  time.Now().Unix(),
+		VideoList: videos,
+		NextTime:  next_time,
 	})
 }
